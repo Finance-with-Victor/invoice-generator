d75f40ebcd55f68f57a7a70f8f4c8d6d5a020441
Victor-Nyangi <gichuivictor@gmail.com>
2024-11-11 19:57:58 +0300

    feat(invoice): download invoice feature

--- src/components/InvoicePreview.tsx
+++ src/components/InvoicePreview.tsx
@@ -1,10 +1,11 @@
+import React, { useRef } from "react";
+   // @ts-ignore
+import { useReactToPrint } from "react-to-print";
+
 import { Card, CardContent } from "@/components/ui/card";
 import { InvoiceData, InvoiceItem } from "@types";
-import React from "react";
 import { ScrollArea } from "./ui/scroll-area";
 import { Button } from "./ui/button";
-import MyDocument from "./BillingDocument";
-import { pdf } from "@react-pdf/renderer";
 
 type InvoicePreviewProps = {
   invoice: InvoiceData;
@@ -12,102 +13,100 @@ type InvoicePreviewProps = {
 const InvoicePreview: React.FC<InvoicePreviewProps> = ({
   invoice,
 }: InvoicePreviewProps) => {
-  const generatePDF = async () => {
-    const blob = await pdf(<MyDocument data={invoice} />).toBlob();
-    // const blob = undefined;
+  const contentRef = useRef<HTMLDivElement>(null);
 
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = "receipt.pdf";
-    link.click();
-  };
+  const reactToPrintFn = useReactToPrint({
+    contentRef,
+  });
 
   return (
     <ScrollArea className="h-[600px]">
       <Card className="w-full bg-white text-black">
-        <CardContent className="p-8 space-y-6">
-          <div className="flex justify-between items-start">
-            <div>
-              <h1 className="text-4xl font-bold text-primary">RECEIPT</h1>
-              <p className="text-sm text-gray-500">
-                Receipt Date: {invoice.date}
+        <CardContent className="space-y-6">
+          <div ref={contentRef} className="p-8">
+            <div className="flex justify-between items-start">
+              <div>
+                <h1 className="text-4xl font-bold text-primary">RECEIPT</h1>
+                <p className="text-sm text-gray-500">
+                  Receipt Date: {invoice.date}
+                </p>
+              </div>
+              <div className="text-right">
+                <h2 className="text-xl font-bold">{invoice.companyName}</h2>
+                <p>{invoice.companyAddress}</p>
+                <p>TEL: {invoice.companyPhone}</p>
+              </div>
+            </div>
+
+            <div className="my-4">
+              <h2 className="text-md font-semibold mb-2">Till No:</h2>
+              <p>{invoice.tillNo}</p>
+            </div>
+
+            <div className="mt-4">
+              <h2 className="text-lg font-semibold mb-2">Bill To:</h2>
+              <p>
+                <span className="font-semibold"></span>
+                {invoice.customerName}
               </p>
             </div>
-            <div className="text-right">
-              <h2 className="text-xl font-bold">{invoice.companyName}</h2>
-              <p>{invoice.companyAddress}</p>
-              <p>TEL: {invoice.companyPhone}</p>
-            </div>
-          </div>
 
-          <div className="my-4">
-            <h2 className="text-md font-semibold mb-2">Till No:</h2>
-            <p>{invoice.tillNo}</p>
-          </div>
-
-          <div className="mt-4">
-            <h2 className="text-lg font-semibold mb-2">Bill To:</h2>
-            <p>
-              <span className="font-semibold"></span>
-              {invoice.customerName}
-            </p>
-          </div>
-
-          <table className="w-full mt-8">
-            <thead>
-              <tr className="border-b border-gray-200">
-                <th className="text-left p-2">#</th>
-                <th className="text-left py-2">Description</th>
-                <th className="text-left py-2">QTY</th>
-                <th className="text-right py-2">@ Ksh</th>
-                <th className="text-right py-2">Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {invoice?.items.map((item: InvoiceItem, index: number) => (
-                <tr key={index} className="border-b border-gray-200">
-                  <td className="py-2">{index + 1}</td>
-                  <td className="py-2 whitespace-pre-line">
-                    {item.description}
-                  </td>
-                  <td className="py-2 text-right">{item.quantity}</td>
-                  <td className="py-2 text-right">
-                    {item.unitPrice?.toFixed(2)}
-                  </td>
-                  <td className="py-2 text-right">
-                    {(item.quantity * item.unitPrice).toFixed(2)}
-                  </td>
+            <table className="w-full mt-8">
+              <thead>
+                <tr className="border-b border-gray-200">
+                  <th className="text-left p-2">#</th>
+                  <th className="text-left py-2">Description</th>
+                  <th className="text-left py-2">QTY</th>
+                  <th className="text-right py-2">@ Ksh</th>
+                  <th className="text-right py-2">Total</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {invoice?.items.map((item: InvoiceItem, index: number) => (
+                  <tr key={index} className="border-b border-gray-200">
+                    <td className="py-2">{index + 1}</td>
+                    <td className="py-2 whitespace-pre-line">
+                      {item.description}
+                    </td>
+                    <td className="py-2 text-right">{item.quantity}</td>
+                    <td className="py-2 text-right">
+                      {item.unitPrice?.toFixed(2)}
+                    </td>
+                    <td className="py-2 text-right">
+                      {(item.quantity * item.unitPrice).toFixed(2)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
 
-          <div className="flex justify-end">
-            <div className="w-1/3 space-y-2">
-              <div className="flex justify-between font-bold">
-                <span>TOTAL</span>
-                <span>KES {invoice.total.toFixed(2)}</span>
+            <div className="flex justify-end">
+              <div className="w-1/3 space-y-2">
+                <div className="flex justify-between font-bold">
+                  <span>TOTAL</span>
+                  <span>KES {invoice.total.toFixed(2)}</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex justify-end">
-            <p>
-              <span className="font-semibold">Amount in words:</span>{" "}
-              {invoice.amountInWords}
-            </p>
+            <div className="flex justify-end">
+              <p>
+                <span className="font-semibold">Amount in words:</span>{" "}
+                {invoice.amountInWords}
+              </p>
+            </div>
+
+            {invoice?.notes ? (
+              <div className="mt-8 space-y-4">
+                <div>
+                  <h3 className="font-semibold mb-2">Notes</h3>
+                  <p className="text-sm">{invoice?.notes}</p>
+                </div>
+              </div>
+            ) : null}
           </div>
 
-          {invoice?.notes ? (
-            <div className="mt-8 space-y-4">
-              <div>
-                <h3 className="font-semibold mb-2">Notes</h3>
-                <p className="text-sm">{invoice?.notes}</p>
-              </div>
-            </div>
-          ) : null}
           <div className="mb-4">
-            <Button onClick={generatePDF}>Generate PDF</Button>
+            <Button onClick={reactToPrintFn}>Generate PDF</Button>
           </div>
         </CardContent>
       </Card>
